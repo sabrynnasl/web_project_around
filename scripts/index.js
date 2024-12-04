847b105a79d73cc15d60fd3c848442bf3ce5d1d7
Sabrynna <sabrynna.mail@gmail.com>
2024-12-03 22:18:25 -0800

    correcao popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,23 @@
 
 // Selecionando os elementos
 const modal = document.getElementById('editProfileModal');
-const addButton = document.querySelector('.profile__add-button');
+const addButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.profile__modal-content-close');
+const profileName = document.querySelector ('.profile__name');
+const profileDescription = document.querySelector ('.profile__description ');
+const modalForm = document.querySelector ('.modal__form');
+
+
 
 // Mostrar o modal
 
 addButton.addEventListener('click', () => {
   modal.classList.add('show');
-  console.log("OI")
+  const name = document.getElementById('name');
+  name.value = profileName.textContent 
+  const role = document.getElementById('role');
+  role.value = profileDescription.textContent
+
 });
 
 // Fechar o modal ao clicar no "X"
@@ -21,4 +30,13 @@ window.addEventListener('click', (event) => {
   if (event.target === modal) {
     modal.classList.remove('show');
   }
+});
+
+modalForm.addEventListener('submit', (event) => {
+event.preventDefault(); 
+const name = document.getElementById('name');
+profileName.textContent = name.value;
+const role = document.getElementById('role');
+profileDescription.textContent = role.value;
+modal.classList.remove('show');
 });
